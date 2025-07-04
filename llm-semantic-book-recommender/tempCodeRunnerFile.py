2041d390d80a0d9f1010ec0172583b265967a031
Ron5866 <142195587+Ron5866@users.noreply.github.com>
2025-07-04 13:08:56 +0530

    Add files via upload

---
+++ llm-semantic-book-recommender/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+
+import gradio as gr
